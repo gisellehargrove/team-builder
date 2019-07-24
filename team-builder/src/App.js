d512921f80cfb69529fd4e0cb6047d86fef961d4
gisellehargrove <gisellehargrove@gmail.com>
2019-07-24 16:06:28 -0400

    building out form component

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,12 +1,24 @@
 import React, { useState } from 'react';
+import Form from './components/Form/Form.js';
 import './App.css';
 
 function App() {
-  const [team, setTeam] = useState({});
-  
+  const [user, setUser] = useState({name: '', email: '', role: ''});
+
+  const handleSubmit = event => {
+    event.preventDefault();
+  };
+
+  const handleChange = event => {
+    setUser({...user, [event.target.name]: event.target.value})
+  };
+
   return (
     <div className="App">
-
+      <Form
+        handleSubmit={handleSubmit}
+        handleChange={handleChange}
+        user={user} />
     </div>
   );
 }
--- team-builder/src/components/Form/Form.js
+++ team-builder/src/components/Form/Form.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
-const Form = () => {
-  return <form></form>
+const Form = (props) => {
+  console.log(props)
+  return (
+    <form onSubmit={event => props.handleSubmit(event)}>
+      <label>
+        Name:
+        <input
+          type="text"
+          value={props.user.name}
+          name="name"
+          onChange={event => props.onChange(event)} />
+      </label>
+
+      <label>
+        Email:
+        <input
+          type="text"
+          value={props.user.email}
+          name="email"
+          onChange={event => props.onChange(event)} />
+      </label>
+    </form>
+
+  );
 };
 
 export default Form;
